5d567b8614d43a78f5ae7752fc9601bd6f34c7fa
ninjamu <nguoiyeumoicuatoi_2005@yahoo.com>
2014-09-13 16:18:44 +0700

    javascript validate

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
 document.getElementById("date").innerHTML = Date();
+function myfunction() {
+	var firt = document.forms["gform"]["firt-input"].value;
+	if (firt="") {
+		alert("Firt name not null");
+		return false;
+	};
+	var last = document.forms["gform"]["last-input"].value;
+	if (last="") {
+		alert("last name not null");
+		return false;
+	};
+	var email = document.forms["gform"]["email-input"].value;
+	var acong=email.indexOf("@");
+	var daucham=email.lastIndexOf(".");
+	if (email="") {
+		alert("Email not null");
+		return false;
+	}
+	else if ((acong<1) || (daucham<acong+2) || (daucham+2>email.length)) {
+		alert("Email Not validate");
+		return false;
+	};
+}
